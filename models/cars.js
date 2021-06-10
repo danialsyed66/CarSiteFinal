c64800a3ec61973120c21f24186f247c04ff7370
danialsyed66 <danialsyed66@users.noreply.github.com>
2021-06-10 11:26:18 +0500

    Update cars.js

--- models/cars.js
+++ models/cars.js
@@ -1,12 +1,24 @@
 var mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
 
 var schema = mongoose.Schema({
-    name: String,
-    model: Number,
-    used: Boolean,
-    price: Number
-})
+  name: String,
+  model: Number,
+  used: Boolean,
+  price: Number,
+});
 
-const cars = mongoose.model("cars", schema);
+const Cars = mongoose.model("Cars", schema);
 
-module.exports = cars;
+function validateCars(data) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    model: Joi.number().min(1700).max(3000).required(),
+    used: Joi.boolean().required(),
+    price: Joi.number().min(0).required(),
+  });
+  return schema.validate(data, { abortEarly: false });
+}
+
+module.exports.Cars = Cars;
+module.exports.validate = validateCars;
