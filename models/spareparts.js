4d04c6f91565dee338cb79848e2d45aaecc72177
danialsyed66 <danialsyed66@users.noreply.github.com>
2021-06-10 11:26:25 +0500

    Update spareparts.js

--- models/spareparts.js
+++ models/spareparts.js
@@ -1,12 +1,24 @@
 var mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
 
 var schema = mongoose.Schema({
-    name: String,
-    company: String,
-    model: Number,
-    price: Number
-})
+  name: String,
+  company: String,
+  model: Number,
+  price: Number,
+});
 
-const spareparts = mongoose.model("spareparts", schema);
+const Spareparts = mongoose.model("Spareparts", schema);
 
-module.exports = spareparts;
+function validateSpareparts(data) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    company: Joi.string().min(3).max(20).required(),
+    model: Joi.number().min(1700).required(),
+    price: Joi.number().min(0).required(),
+  });
+  return schema.validate(data, { abortEarly: false });
+}
+
+module.exports.Spareparts = Spareparts;
+module.exports.validate = validateSpareparts;
