d8b4ffd1489766343704c1830145a6910dc0ff70
danialsyed66 <danialsyed66@users.noreply.github.com>
2021-06-10 11:27:13 +0500

    Create cars.js

---
+++ routes/api/cars.js
@@ -0,0 +1,50 @@
+const express = require("express");
+let router = express.Router();
+const validateCars = require("../../middlewares/validateCars");
+var { Cars } = require("../../models/cars");
+
+//get cars
+router.get("/", async (req, res) => {
+  let page = Number(req.query.page ? req.query.page : 1);
+  let perPage = Number(req.query.perPage ? req.query.perPage : 10);
+  let skipRecords = perPage * (page - 1);
+  let cars = await Cars.find().skip(skipRecords).limit(perPage);
+  return res.send(cars);
+});
+//get single cars
+router.get("/:id", async (req, res) => {
+  try {
+    let car = await Cars.findById(req.params.id);
+    if (!car)
+      return res.status(400).send("Car With given ID is not present"); //when id is not present id db
+    return res.send(car); //everything is ok
+  } catch (err) {
+    return res.status(400).send("Invalid ID"); // format of id is not correct
+  }
+});
+//update a record
+router.put("/:id", validateCars, async (req, res) => {
+  let car = await Cars.findById(req.params.id);
+  car.name = req.body.name;
+  car.model = req.body.model;
+  car.used = req.body.used;
+  car.price = req.body.price;
+  await car.save();
+  return res.send(car);
+});
+//update a record
+router.delete("/:id", async (req, res) => {
+  let car = await Cars.findByIdAndDelete(req.params.id);
+  return res.send(car);
+});
+//Insert a record
+router.post("/", validateCars, async (req, res) => {
+  let car = new Cars();
+  car.name = req.body.name;
+  car.model = req.body.model;
+  car.used = req.body.used;
+  car.price = req.body.price;
+  await car.save();
+  return res.send(car);
+});
+module.exports = router;
