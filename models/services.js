4ec1ce912cc98cd9719f7db98ac1a807a7ce6f0e
danialsyed66 <danialsyed66@users.noreply.github.com>
2021-06-10 11:26:22 +0500

    Update services.js

--- models/services.js
+++ models/services.js
@@ -1,10 +1,20 @@
 var mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
 
 var schema = mongoose.Schema({
-    name: String,
-    price: Number
-})
+  name: String,
+  price: Number,
+});
 
-const services = mongoose.model("services", schema);
+const Services = mongoose.model("Services", schema);
 
-module.exports = services;
+function validateServices(data) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    price: Joi.number().min(0).required(),
+  });
+  return schema.validate(data, { abortEarly: false });
+}
+
+module.exports.Services = Services;
+module.exports.validate = validateServices;
